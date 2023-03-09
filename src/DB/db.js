067f0fdec008f39961c7f5ad8971abc3298922dd
Aldo Hernandez <aldo2588@gmail.com>
2023-03-09 12:18:50 -0600

    fix bug

--- src/DB/db.js
+++ src/DB/db.js
@@ -9,9 +9,9 @@ import {
  } from '../config.js'
 
 export const pool = createPool({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASS,
-    port: DB_PORT,
-    database: DB_DATABASE
+    host: 'containers-us-west-175.railway.app',
+    user: 'root',
+    password: 'zU8CEzj5YAg6WRIcAXw2',
+    port: 6708,
+    database: 'railway'
 })
